1a4ce0df735b407db64dcd0d68277279cb5611ba
Kyrylo OLIINYK <kyrylo.oliinyk@smile-ukraine.com>
2024-08-05 13:24:25 +0300

    add linter

--- flask/app.py
+++ flask/app.py
@@ -1,12 +1,12 @@
 from flask import Flask
 app = Flask(__name__)
-content = 'content'
+CONTENT = 'content'
 
 
 @app.route('/')
 def return_content():
-    return content
+    return CONTENT
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-
