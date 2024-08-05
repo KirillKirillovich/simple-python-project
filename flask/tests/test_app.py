d8d1383e667770e771adc29e719ea8ed5c53c435
Kyrylo OLIINYK <kyrylo.oliinyk@smile-ukraine.com>
2024-08-05 17:23:11 +0300

    add linter config file

--- flask/tests/test_app.py
+++ flask/tests/test_app.py
@@ -3,9 +3,7 @@ import os
 import pytest
 from app import app
 
-sys.path.insert(
-    0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-)
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 
 @pytest.fixture
